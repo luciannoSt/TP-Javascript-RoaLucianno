4176568a402bcc23109b9ee521a814d6faedf506
luciannoSt <luciannotwt@gmail.com>
2024-05-03 10:21:44 -0300

    se añade verificacion de orden al codigo

--- assets/js/numAscendente.js
+++ assets/js/numAscendente.js
@@ -2,24 +2,24 @@ let num1=parseInt(prompt("ingrese el primer numero"));
 let num2=parseInt(prompt("ingrese el segundo numero"));
 let num3=parseInt(prompt("ingrese el tercer numero"));
 let temp1, temp2;
-
-// Order the first two numbers
-if (num1 > num2) {
-  temp1 = num1;
-  num1 = num2;
-  num2 = temp1;
+if (num1 > num2 && num1>num3) {
+        if(num2>num3){
+            console.log(num1,num2,num3);
+        }else{
+            console.log(num1,num3,num2);
+        }
+}else if(num2>num1 && num2>num3){
+        if(num1>num3){
+            console.log(num2,num1,num3);
+        }else{
+            console.log(num2,num3,num1);
+        }
+}else if(num3>num1 && num3>num1){
+        if(num2>num1){
+            console.log(num3,num2,num1);
+        }else{
+            console.log(num3,num1,num2);
+        }
+}else if(num1==num2 && num3==num1){
+        console.log("numeros iguales:");
 }
-
-// Check the third number and swap accordingly
-if (num3 < num1) {
-  temp2 = num3;
-  num3 = num1;
-  num1 = temp2;
-} else if (num3 < num2) {
-  temp2 = num3;
-  num3 = num2;
-  num2 = temp2;
-}
-
-console.log("Números ordenados en orden ascendente:");
-console.log(num1,num2,num3);
