90531131c0a5c15e6cd1ddced4af42ba5fe29e72
luciannoSt <luciannotwt@gmail.com>
2024-05-03 09:51:27 -0300

    se realiza la verificacion de datos con isNaN

--- assets/js/parImpar.js
+++ assets/js/parImpar.js
@@ -1,16 +1,17 @@
 let c=0;
-let n=parseInt("ingrese un numero");
 while(c<1){
-    if(n==Number){
+    let n=parseInt(prompt("ingrese un numero"));
+    tipo=typeof n;
+    if(tipo=="number"){
         if(n%2==0){
             console.log("el numero es par");
             c=1;
-        }else{
+        }else if(n%2==1){
             console.log("el numero es impar");
             c=1;
         }
     }
-    else{
+    else if(tipo==isNaN){
         console.log("el numero no es un numero");
     }
 }
