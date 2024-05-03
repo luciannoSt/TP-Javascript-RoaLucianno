abfdf919cd104929ab76a23addb7e398e76be7eb
luciannoSt <luciannotwt@gmail.com>
2024-05-03 09:42:35 -0300

    se hace la verificacion de entrada de datos y se vincula con el html

--- assets/js/parImpar.js
+++ assets/js/parImpar.js
@@ -1,7 +1,18 @@
+let c=0;
 let n=parseInt("ingrese un numero");
-
-if(n%2==0){
-    console.log("el numero es par");
-}else{
-    console.log("el numero es impar");
+while(c<1){
+    if(n==Number){
+        if(n%2==0){
+            console.log("el numero es par");
+            c=1;
+        }else{
+            console.log("el numero es impar");
+            c=1;
+        }
+    }
+    else{
+        console.log("el numero no es un numero");
+    }
 }
+
+
