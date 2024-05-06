5dc80f9d59f1fe2813f50241a8504892d1d0165b
luciannoSt <luciannotwt@gmail.com>
2024-05-06 09:21:56 -0300

    se cambia el orden de ascendente

--- assets/js/numAscendente.js
+++ assets/js/numAscendente.js
@@ -4,21 +4,21 @@ let num3=parseInt(prompt("ingrese el tercer numero"));
 let temp1, temp2;
 if (num1 > num2 && num1>num3) {
         if(num2>num3){
-            console.log(num1,num2,num3);
-        }else{
-            console.log(num1,num3,num2);
-        }
-}else if(num2>num1 && num2>num3){
-        if(num1>num3){
-            console.log(num2,num1,num3);
+            console.log(num3,num2,num1);
         }else{
             console.log(num2,num3,num1);
         }
+}else if(num2>num1 && num2>num3){
+        if(num1>num3){
+            console.log(num3,num1,num2);
+        }else{
+            console.log(num1,num3,num2);
+        }
 }else if(num3>num1 && num3>num1){
         if(num2>num1){
-            console.log(num3,num2,num1);
+            console.log(num1,num2,num3);
         }else{
-            console.log(num3,num1,num2);
+            console.log(num2,num1,num3);
         }
 }else if(num1==num2 && num3==num1){
         console.log("numeros iguales:");
